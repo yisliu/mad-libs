a07c22c2c023c119838f30d86501a08b3159cffa
Yisi Liu <>
2021-06-02 17:54:06 +0000

    added inputs

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
-b
+
 while True():
-  nouns = inpu
+  nouns = input("Input a noun: ")
+  number = input("Input a number: ")
+  verb = input("Input a verb: ")
+  adj = input("Input a adjective: ")
+  place = input("Input the name of a place: ")
+  name = input("Input a name: ")
+  food = input("Input your favorite food: ")
+  
