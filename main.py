1b2812eb35e0bc358a4d7dbf108a2270021318df
Yisi Liu <>
2021-06-02 18:06:07 +0000

    I finish the project.

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+game = 0
 
-while True():
+while game < 1:
+  print("You well recive a serious of inputs, to creat a story.")
   nouns = input("Input a noun: ")
   number = input("Input a number: ")
   verb = input("Input a verb: ")
@@ -7,4 +9,10 @@ while True():
   place = input("Input the name of a place: ")
   name = input("Input a name: ")
   food = input("Input your favorite food: ")
-  
+  print("Look there is " + nouns)
+  print("There is " + number + " " + food)
+  print("You " + verb + " to exercise")
+  print("I have a " + adj + " " + nouns)
+  print("I live in a " + place)
+  print("The name of my puppy is " + name)
+  print("My favorite food is " + food)
